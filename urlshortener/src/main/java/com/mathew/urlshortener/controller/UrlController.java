bb46759cf02214d061473c4e8b4dcfbf55a91048
Mathew Lobo <mathewjohnlobo25@gmail.com>
2026-04-07 12:03:16 -0400

    fix: use SPRING_APPLICATION_JSON for base URL config

--- urlshortener/src/main/java/com/mathew/urlshortener/controller/UrlController.java
+++ urlshortener/src/main/java/com/mathew/urlshortener/controller/UrlController.java
@@ -15,7 +15,7 @@ public class UrlController {
 
     private final UrlService urlService;
 
-    @Value("${APP_BASE_URL:http://localhost:8080}")
+    @Value("${app.base-url:http://localhost:8080}")
     private String baseUrl;
 
     public UrlController(UrlService urlService) {
