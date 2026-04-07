9da227f3f287f368288f936fb42ef6c826b61c99
Mathew Lobo <mathewjohnlobo25@gmail.com>
2026-04-06 22:22:26 -0400

    chore: clean up project structure

--- src/main/java/com/mathew/urlshortener/exception/UrlExpiredException.java
+++
@@ -1,7 +0,0 @@
-package com.mathew.urlshortener.exception;
-
-public class UrlExpiredException extends RuntimeException {
-    public UrlExpiredException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/mathew/urlshortener/exception/UrlNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.mathew.urlshortener.exception;
-
-public class UrlNotFoundException extends RuntimeException {
-    public UrlNotFoundException(String message) {
-        super(message);
-    }
-}
